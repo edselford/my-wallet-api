1ff47a032737e9338f42ea772343b19fabbde6c2
edselford <edselmustapa@gmail.com>
2023-10-29 21:19:18 +0700

    fix transaction api

--- app/api/v1/transaction/route.ts
+++ app/api/v1/transaction/route.ts
@@ -1,4 +1,5 @@
 import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -7,7 +8,7 @@ export async function POST(req: Request) {
         const db = client.db(process.env.MONGODB_DATABASE);
 
         const body = await req.json();
-        let user_id;
+        let user_id: ObjectId;
 
         if (body.email != null) {
             let user = await db.collection("users").findOne({
@@ -17,7 +18,7 @@ export async function POST(req: Request) {
             if (user == null) return new NextResponse("user not found", {status: 400});
             user_id = user._id
         } else {
-            user_id = body.user_id
+            user_id = new ObjectId(body.user_id)
         }
 
         const transaction = await db.collection("transactions").find({
@@ -27,7 +28,9 @@ export async function POST(req: Request) {
             ],
         }).toArray();
 
-        return NextResponse.json(transaction);
+        return NextResponse.json(transaction.map(transaction => {
+            return {...transaction, isSender: transaction.sender == user_id.toString()};
+        }));
     } catch (e) {
         return new NextResponse("", { status: 400 });
     }
